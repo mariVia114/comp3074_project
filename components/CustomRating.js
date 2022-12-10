95c1cc0d0fe83d9a84cb8802c81b2c1f377bc31d
janinene <95052472+janinene@users.noreply.github.com>
2022-12-10 01:03:27 -0500

    update using ionicons, pls add "react-native-vector-icons/Ionicons": "^4.2.4" inside package json

--- components/CustomRating.js
+++ components/CustomRating.js
@@ -3,29 +3,40 @@ import React, {useState} from "react";
 import { Text, StyleSheet, ScrollView, Keyboard, TextInput, View, TouchableOpacity, Image, Alert } from "react-native";
 import * as Style from "../assets/styles";
 import {styles} from "./Restaurant";
+import { Ionicons } from '@expo/vector-icons';
 
 
     
-    const CustomRating = () =>{
+const CustomRating = () =>{
 
-        const [defaultRating, setDefaultRating] = useState(2)
-    const [maxRating, setMaxRating] = useState([1,2,3,4,5])
-    const starImgFilled = 'https://raw.githubusercontent.com/tranhonghan/images/main/star_filled.png'
-    const starImgCorner =  'https://raw.githubusercontent.com/tranhonghan/images/main/star_corner.png'
-        return(
-            <View style={styles.customRating}>
-                {
-                    maxRating.map((item, key) => {
-                        return(
-                            <TouchableOpacity activeOpacity={0.7} key={item} onPress={() => setDefaultRating(item)}>
-                                <Image style={styles.starImgStyle} source={item<=defaultRating ? {uri:starImgFilled} : {uri:starImgCorner}}/>
-                            </TouchableOpacity>
-                        )
-                    })
-                }
+    const [defaultRating, setDefaultRating] = useState(2)
+const [maxRating, setMaxRating] = useState([1,2,3,4,5])
+    return(      
+        <View style={rating.customRating}>      
+            {
+                maxRating.map((item) => {
+                    return(
+                        <TouchableOpacity activeOpacity={0.7} key={item} onPress={() => setDefaultRating(item)}>
+                            <Ionicons style={rating.starImgStyle} name= { item<=defaultRating ? 'star' :  'star-outline'} />
+                        </TouchableOpacity>
+                    )
+                })
+            }
+        </View> 
+       
+    )
+}
 
-            </View>
-        )
+const rating = StyleSheet.create ( {
+    customRating:{
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'nowrap',
+    },
+    starImgStyle:{
+        fontSize: 50,
+        color: '#57e32c'
     }
+})
 
-    export default CustomRating;
+export default CustomRating;
