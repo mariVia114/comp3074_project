017b3f2887e338ddc0ae2eebdc4f0d8f52014acb
janinene <95052472+janinene@users.noreply.github.com>
2022-12-12 19:42:40 -0500

    Update EditRestaurant.js

--- components/EditRestaurant.js
+++ components/EditRestaurant.js
@@ -18,54 +18,49 @@ import {
 } from "@ui-kitten/components";
 import { TouchableWithoutFeedback } from "@ui-kitten/components/devsupport";
 import {styles} from './AddRestaurant';
-import CustomRating from "./CustomRating";
+import { Ionicons } from '@expo/vector-icons';
 
 
 function EditRestaurant({route, navigation, ...props}) {
-    const {i, r} = route.params;
-    console.log(r)
-    const[editName, setEditName] = useState(r.newRestaurantName);
-    const[editAddy, setEditAddy] = useState(r.newRestaurantAddy);
-    const[editPhone, setEditPhone] = useState(r.newRestaurantPhone);
-    const[editTag, setEditTag] = useState(r.newRestaurantTags);
-    const[editDesc, setEditDesc] = useState(r.newRestaurantDesc);
-    const[editRate, setEditRate] = useState(r.newRestaurantRate);
-    console.log(...props.restaurants)
+    const {i} = route.params;
+    const[restaurantName, setEditName] = useState(props.restaurants[i].restaurantName);
+    const[restaurantAddy, setEditAddy] = useState(props.restaurants[i].restaurantAddy);
+    const[restaurantPhone, setEditPhone] = useState(props.restaurants[i].restaurantPhone);
+    const[restaurantTags, setEditTag] = useState(props.restaurants[i].restaurantTags);
+    const[restaurantDesc, setEditDesc] = useState(props.restaurants[i].restaurantDesc);
+    const[restaurantRate, setEditRate] = useState(props.restaurants[i].restaurantRate);
+
     function editRestaurant(){
-        let edited = [...props.restaurants];
-        edited[i] = [{editName,editAddy,editPhone,editTag,editDesc,editRate}];
+        let edited = props.restaurants;
+        edited[i] = {restaurantName,restaurantAddy,restaurantPhone,restaurantTags,restaurantDesc,restaurantRate}
         props.setRestaurants(edited);
-        navigation.navigate('Restaurant');
+        navigation.navigate('RestaurantDetails', {id: i});
     }
-
-
-
   return (
     <ScrollView>
             <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
                 <View style={{padding:20, justifyContent:'space-evenly'}}>
-                    <TextInput style={[styles.input]} placeholder='Enter Name' value={editName.toString()} onChangeText={(text)=>setEditName(text)} ></TextInput>
-                    <TextInput style={[styles.input]} placeholder='Enter Address...' value={editAddy.toString()} onChangeText={(text)=>setEditAddy(text)} ></TextInput>
-                    <TextInput style={[styles.input]} placeholder='Phone Number...' value={editPhone.toString()} onChangeText={(text)=>setEditPhone(text)}></TextInput>
-                    <TextInput style={[styles.input]} placeholder='Tag ie;#vegan' value={editTag.toString()} onChangeText={(text)=>setEditTag(text)}></TextInput>
-                    <TextInput style={[styles.input, {height: 80} ]} placeholder='Description' value={editDesc.toString()} onChangeText={(text)=>setEditDesc(text)}></TextInput>
+                    <TextInput style={[styles.input]} placeholder='Enter Name' value={restaurantName.toString()} onChangeText={(text)=>setEditName(text)} ></TextInput>
+                    <TextInput style={[styles.input]} placeholder='Enter Address...' value={restaurantAddy.toString()} onChangeText={(text)=>setEditAddy(text)} ></TextInput>
+                    <TextInput style={[styles.input]} placeholder='Phone Number...' value={restaurantPhone.toString()} onChangeText={(text)=>setEditPhone(text)}></TextInput>
+                    <TextInput style={[styles.input]} placeholder='Tag ie;#vegan' value={restaurantTags.toString()} onChangeText={(text)=>setEditTag(text)}></TextInput>
+                    <TextInput style={[styles.input, {height: 80} ]} placeholder='Description' value={restaurantDesc.toString()} onChangeText={(text)=>setEditDesc(text)}></TextInput>
                     <Text style={styles.ratingText}>Rating:  </Text>
-                    <CustomRating style={{paddingTop:0}} value={editRate} onPress={(text) => props.setRestaurantRate(text)}/>
-                    <TouchableOpacity style={styles.button} onPress={() => {
-                        if(props.restaurantName===''){
-                            Alert.alert('Please type something');
-                        }else if(props.restaurantAddy===''){
-                            Alert.alert('Please type something');
-                        }else if(props.restaurantPhone===''){
-                            Alert.alert('Please type something');
-                        }else if(props.restaurantTags===''){
-                            Alert.alert('Please type something');
-                        }else if(props.restaurantDesc===''){
-                            Alert.alert('Please type something');
-                        }
-                        else{
-                            editRestaurant();
-                        }}}>
+                    {
+                         <View style={styles.customRating}>      
+                         {
+                             ['1★','2★','3★','4★','5★'].map((item) => {
+                                 return(
+                                     <TouchableOpacity activeOpacity={0.7} key={item} value={restaurantRate.toString()} onPress={(text) =>  props.setRestaurantRate(text)}>
+                                         <Ionicons style={styles.starImgStyle} name= { item<= restaurantRate ? 'star' :  'star-outline'} />
+                                     </TouchableOpacity>
+                                 )
+                             })
+                         }
+                     </View> 
+                    }
+                    {/* <CustomRating style={{paddingTop:0}} value={restaurantRate} onPress={(text) => setEditRate(text)}/> */}
+                    <TouchableOpacity style={styles.button} onPress={() => {editRestaurant(); }}>
                         <Text style={styles.buttonText}>Save</Text></TouchableOpacity>
                 </View>
             </TouchableWithoutFeedback>
